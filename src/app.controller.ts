5307a2bbbe6922f8ec0e047e206b19c6eb8155d1
hiep.hoang <hiep.hoang@neopay.vn>
2025-08-14 10:17:19 +0700

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Query, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+
+@Controller('math')
+export class AppController {
+  constructor(@Inject('MATH_SERVICE') private readonly client: ClientProxy) {}
+
+  @Get('sum')
+  async sum(@Query('a') a: number, @Query('b') b: number) {
+    const result = await firstValueFrom(
+      this.client.send({ cmd: 'sum' }, [Number(a), Number(b)]),
+    );
+    return { result };
+  }
+}
