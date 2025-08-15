10bfc01e2c3d8429d924d5c423cdc9064c294630
hiep.hoang <hiep.hoang@neopay.vn>
2025-08-15 16:29:26 +0700

    test trigger build

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,6 +11,6 @@ export class AppController {
     const result = await firstValueFrom(
       this.client.send({ cmd: 'sum' }, [Number(a), Number(b)]),
     );
-    return { result: 'TEST CHANGE' };
+    return { result: 'TEST CHANGE 2' };
   }
 }
