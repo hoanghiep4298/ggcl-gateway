ec3bc7e24a4d36938d5b619cf9899a6b1be8948d
hiep.hoang <hiep.hoang@neopay.vn>
2025-08-28 14:53:03 +0700

    up

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { AppService } from './app.service';
       {
         name: 'MATH_SERVICE',
         transport: Transport.TCP,
-        options: { host: '127.0.0.1', port: 3001 },
+        options: { host: '0.0.0.0', port: 3001 },
       },
     ]),
   ],
